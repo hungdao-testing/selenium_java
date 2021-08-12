458f59e369dcc8449b5d0f172e46948eda559e7e
hungdao-testing <daoquanghunggtvt@gmail.com>
2021-08-12 21:23:10 +0700

    rename test case

--- src/test/java/com/aspire/loan/tests/RegisterTest.java
+++ src/test/java/com/aspire/loan/tests/RegisterTest.java
@@ -6,8 +6,6 @@ import com.aspire.loan.utilfunctions.OtpHandlePage;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
-
 
 public class RegisterTest extends BaseTest{
 
@@ -19,7 +17,7 @@ public class RegisterTest extends BaseTest{
     }
 
     @Test
-    public void test_open_register_page() throws IOException {
+    public void verify_client_could_register_a_new_account_with_valid_data(){
 
         this.signUpPage.goTo().isAt();
         this.signUpPage
@@ -27,7 +25,6 @@ public class RegisterTest extends BaseTest{
 
         OtpHandlePage otpHandlePage = new OtpHandlePage(driver, DataManagement.getPersonal().getPhone());
         otpHandlePage.isAtOtpScreen().inputOtp("1234");
-
 
     }
 }
