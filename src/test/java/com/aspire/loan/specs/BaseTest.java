57daf12cbf82600668cde00382f0d6a4106099de
hungdao-testing <daoquanghunggtvt@gmail.com>
2021-08-14 23:47:50 +0700

    implement driver_factory

--- src/test/java/com/aspire/loan/specs/BaseTest.java
+++ src/test/java/com/aspire/loan/specs/BaseTest.java
@@ -1,5 +1,7 @@
 package com.aspire.loan.specs;
 
+import com.aspire.loan.config.AppConfig;
+import com.aspire.loan.config.DriverFactory;
 import com.aspire.loan.config.GlobalConstants;
 import com.aspire.loan.data.DataManagement;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -8,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import java.util.concurrent.TimeUnit;
 
@@ -17,22 +20,14 @@ public class BaseTest {
 
     @BeforeTest
     public void setupClass()  {
-        ChromeOptions chromeOptions = new ChromeOptions();
-
-        /*
-        * https://www.selenium.dev/documentation/en/webdriver/page_loading_strategy/
-        * */
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver(chromeOptions);
+        driver = DriverFactory.getDriver(AppConfig.getBrowserName());
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(GlobalConstants.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
     }
 
-    @AfterClass
+    @AfterTest
     public void teardownClass() {
         if (driver != null) {
-            driver.close();
             driver.quit();
         }
     }
